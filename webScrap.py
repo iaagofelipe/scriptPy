948c331f57d6dc382a5fe00f026bdf9505712122
Iago Felipe Morais da Silva <iagosilva@mobitbrasil.com.br>
2021-12-21 11:47:09 -0300

    refactoring code

--- webScrap.py
+++ webScrap.py
@@ -3,35 +3,46 @@ from datetime import datetime, timedelta
 import re
 import requests
 
-try:
-  file = urlopen(r"http://10.85.20.8/maj/rsfilemanager.log")
-except requests.Timeout as err:
-  print("DEU ERRO AQUI " + err)
 
 important = []
 keep_phrases = ["Finalizando importacao lote"]
-
 max_timed_out = timedelta(minutes=10)
+url =r'http://10.85.20.8/maj/rsfilemanager.log'
 
+def search_file():
+    try:
+      file = urlopen(url)
+    except requests.Timeout as err:
+      print("DEU ERRO AQUI " + err)
+    return file
 
-for line in file:
-  for phrase in keep_phrases:
-    if phrase in line.decode("utf-8"):
-      important.append(line.decode("utf-8"))
-      break
+file = search_file()
 
-log_text = important[len(important)-1]
-matches = re.findall(r'(\d+/\d+/\d+ \d+:\d+:\d+)',log_text)
+def scrap_file(important, keep_phrases, file):
+    for line in file:
+      for phrase in keep_phrases:
+        if phrase in line.decode("utf-8"):
+          important.append(line.decode("utf-8"))
+          break
 
-date_time_str = matches[0]
-date_time_obj = datetime.strptime(date_time_str, '%Y/%m/%d %H:%M:%S')
+    log_text = important[len(important)-1]
+    matches = re.findall(r'(\d+/\d+/\d+ \d+:\d+:\d+)',log_text)
 
-date_time_difference =  datetime.today() - date_time_obj
+    date_time_str = matches[0]
+    date_time_obj = datetime.strptime(date_time_str, '%Y/%m/%d %H:%M:%S')
 
-print('Data atual: ', datetime.today() )
-print('Dia do ultimo log: ',date_time_str)
-print('Tempo entre o ultimo log e a data atual: ', date_time_difference)
-print(log_text)
-print('\n')
-if (date_time_difference >= max_timed_out):
-  print("Erro, tempo entre ultimo log e data atual maior que: ", max_timed_out, )
+    date_time_difference =  datetime.today() - date_time_obj
+    return log_text,date_time_str,date_time_difference
+
+log_text, date_time_str, date_time_difference = scrap_file(important, keep_phrases, file)
+
+def show_results(max_timed_out, log_text, date_time_str, date_time_difference):
+    print('Data atual: ', datetime.today() )
+    print('Dia do ultimo log: ',date_time_str)
+    print('Tempo entre o ultimo log e a data atual: ', date_time_difference)
+    print(log_text)
+    print('\n')
+    if (date_time_difference >= max_timed_out):
+      print("Erro, tempo entre ultimo log e data atual maior que: ", max_timed_out, )
+
+show_results(max_timed_out, log_text, date_time_str, date_time_difference)
